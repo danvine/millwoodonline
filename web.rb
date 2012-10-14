18a2ea099f46a1f948b6d7c7881c53a8615fddb8
Tim Millwood <timmillwood@ubuntu.(none)>
2012-10-14 22:27:49 +0100

    updating DB

--- web.rb
+++ web.rb
@@ -6,7 +6,7 @@ require "dm-migrations"
 require "digest/sha1"
 require 'rack-flash'
 require "sinatra-authentication"
-DataMapper.setup(:default, 'postgres://lbhhmtafaowdgx:tpjR5sVtWEswPaJ9tsQ7q-_cdj@ec2-54-243-233-216.compute-1.amazonaws.com:5432/d9r9mjl2refokn')
+DataMapper.setup(:default, 'sqlite::memory:')
 DataMapper.auto_upgrade!
 use Rack::Session::Cookie, :secret => 'superdupersecret'
 use Rack::Flash
