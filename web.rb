c3463728056dd81bf7f53f4a3298b00630da0bcc
Tim Millwood <timmillwood@ubuntu.(none)>
2012-10-15 15:47:49 +0100

    bootstrapping the login form

--- web.rb
+++ web.rb
@@ -3,12 +3,12 @@ require 'sinatra'
 require "dm-core"
 require "dm-migrations"
 require "digest/sha1"
-require 'rack-flash'
+#require 'rack-flash'
 require "sinatra-authentication"
 DataMapper.setup(:default, 'postgres://lbhhmtafaowdgx:tpjR5sVtWEswPaJ9tsQ7q-_cdj@ec2-54-243-233-216.compute-1.amazonaws.com:5432/d9r9mjl2refokn')
 DataMapper.auto_upgrade!
 use Rack::Session::Cookie, :secret => 'superdupersecret'
-use Rack::Flash
+#use Rack::Flash
 
 configure do
     set :static, true
