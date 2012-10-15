5208013b3922c58ff0888d1eb54619d7bda598b3
Tim Millwood <timmillwood@ubuntu.(none)>
2012-10-15 13:46:54 +0100

    Adding erb views for authentication

--- web.rb
+++ web.rb
@@ -3,20 +3,20 @@ require 'sinatra'
 require "dm-core"
 require "dm-migrations"
 require "digest/sha1"
-require 'rack-flash'
+#require 'rack-flash'
 require "sinatra-authentication"
 DataMapper.setup(:default, 'postgres://lbhhmtafaowdgx:tpjR5sVtWEswPaJ9tsQ7q-_cdj@ec2-54-243-233-216.compute-1.amazonaws.com:5432/d9r9mjl2refokn')
 DataMapper.auto_upgrade!
 use Rack::Session::Cookie, :secret => 'superdupersecret'
-use Rack::Flash
+#use Rack::Flash
 
 configure do
-    #set :template_engine, :erb
-    #set :sinatra_authentication_view_path, Pathname(__FILE__).dirname.expand_path + "views/"
+    set :template_engine, :erb
+    set :sinatra_authentication_view_path, Pathname(__FILE__).dirname.expand_path + "views/"
 end
 
 get '/' do
   @title = 'Millwood Online'
-  @user = current_user.name if logged_in? 
+  @user = current_user
   erb :home
 end
