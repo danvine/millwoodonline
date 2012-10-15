5af02b3df330f98e9e6628c91afee3cee918071d
Tim Millwood <timmillwood@ubuntu.(none)>
2012-10-15 09:25:38 +0100

    Adding rack flash3

--- Gemfile
+++ Gemfile
@@ -1,9 +1,9 @@
 source :rubygems
 gem 'sinatra', '1.1.0'
-gem 'thin'
+gem 'thin', '1.5.0'
 gem 'haml'
 gem "dm-core"
 gem "dm-migrations"
 gem 'dm-postgres-adapter'
-gem 'rack-flash'
+gem 'rack-flash3'
 gem "sinatra-authentication"
--- web.rb
+++ web.rb
@@ -4,9 +4,9 @@ require 'haml'
 require "dm-core"
 require "dm-migrations"
 require "digest/sha1"
-#require 'rack-flash'
+require 'rack-flash'
 require "sinatra-authentication"
 DataMapper.setup(:default, 'postgres://lbhhmtafaowdgx:tpjR5sVtWEswPaJ9tsQ7q-_cdj@ec2-54-243-233-216.compute-1.amazonaws.com:5432/d9r9mjl2refokn')
 DataMapper.auto_upgrade!
 use Rack::Session::Cookie, :secret => 'superdupersecret'
-#use Rack::Flash
+use Rack::Flash
