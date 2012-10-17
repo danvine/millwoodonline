b5b8d09c19979baa2d60bc38170b6e9961d636e6
Tim Millwood <timmillwood@ubuntu.(none)>
2012-10-17 16:53:53 +0100

    Fixing sloppy erb

--- web.rb
+++ web.rb
@@ -7,7 +7,7 @@ require 'sinatra-authentication'
 require 'sanitize'
 require 'pony'
 
-DataMapper.setup(:default, 'postgres://lbhhmtafaowdgx:tpjR5sVtWEswPaJ9tsQ7q-_cdj@ec2-54-243-233-216.compute-1.amazonaws.com:5432/d9r9mjl2refokn')
+DataMapper.setup(:default, ENV['HEROKU_POSTGRESQL_SILVER_URL'])
       class Content
         include DataMapper::Resource
 
