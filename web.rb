2c9c9c7cf239a0219bf3a5bbe9ec336d5e54f1cc
Tim Millwood <timmillwood@ubuntu.(none)>
2012-10-22 14:51:01 +0100

    Adding a few touches

--- web.rb
+++ web.rb
@@ -145,6 +145,7 @@ end
 
 # Feeds
 get '/taxonomy/term/25/0/feed' do
+  content_type 'text/xml; charset=utf8'
   tag = '%drupal%'
   @contents = Content.all(:type => 'blog', :tags.like => tag, :order => [ :created.desc ])
   
@@ -152,6 +153,7 @@ get '/taxonomy/term/25/0/feed' do
 end
 
 get '/rss.xml' do
+  content_type 'text/xml; charset=utf8'
   @contents = Content.all(:type => 'blog', :order => [ :created.desc ])
   
   builder :rss
