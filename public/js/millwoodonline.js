0643b708adf0b42394ce54475e3427c65bfcb7d0
Tim Millwood <timmillwood@ubuntu.(none)>
2012-10-26 11:42:24 +0100

    Updating tweets

--- public/js/millwoodonline.js
+++ public/js/millwoodonline.js
@@ -4,14 +4,27 @@ if ($('#tweets').length) {
     $.each(data, function(key, value) {
 	  if(key < 3) {
 		  console.log(value);
-        $('#tweets').append('<li><img src="' + value.user.profile_image_url + '" style="float: left; padding-right: 10px">' + replaceURLWithHTMLLinks(value.text) + '<hr></li>');
+        $('#tweets').append('<li><img src="' + value.user.profile_image_url + '" style="float: left; padding-right: 10px">' + value.text.linkify().atify().hashify() + '<hr></li>');
       }
     });
   });
 }
 });
 
-function replaceURLWithHTMLLinks(text) {
-    var exp = /(\b(https?|ftp|file):\/\/[-A-Z0-9+&@#\/%?=~_|!:,.;]*[-A-Z0-9+&@#\/%=~_|])/ig;
-    return text.replace(exp,"<a href='$1'>$1</a>"); 
-}
+     String.prototype.hashify = function() {
+            return this.replace(/#([A-Za-z0-9\/\.]*)/g, function(m) {
+                return '<a target="_new" href="http://twitter.com/search?q=' + m.replace('#','') + '">' + m + "</a>";
+            });
+        };
+
+        String.prototype.linkify = function(){
+            return this.replace(/[A-Za-z]+:\/\/[A-Za-z0-9-_]+\.[A-Za-z0-9-_:%&\?\/.=]+/, function(m) {
+                return m.link(m);
+            });
+        };
+
+        String.prototype.atify = function() {
+            return this.replace(/@[\w]+/g, function(m) {
+                return '<a href="http://www.twitter.com/' + m.replace('@','') + '">' + m + "</a>";
+            });
+        };
