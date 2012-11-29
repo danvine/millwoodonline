34b42829ff5cc74bdf1dcf781fa28c2428e93f2d
Tim Millwood <tim@millwoodonline.co.uk>
2012-11-29 17:03:53 +0000

    Adding better 404 page

--- routes/errors.rb
+++ routes/errors.rb
@@ -1,7 +1,7 @@
 not_found do
-  @title = "404"
+  @title = "404 Page not found"
   @description = "Error: 404 - Millwood Online"
-  erb "<h2>Page not found</h2>"
+  erb '<h2>Enjoy this Eric Clapton video instead:</h2><div style="position:relative;padding-bottom:56.25%;padding-top: 30px;height: 0;overflow: hidden;"><iframe src="https://www.youtube-nocookie.com/embed/l4hv_8TXFWg?rel=0&autoplay=1&iv_load_policy=3&loop=1&showinfo=0&modestbranding=1&controls=0" frameborder="0" allowfullscreen style="position:absolute;top:0;left:0;width:100%;height:100%;"></iframe></div>'
 end
 
 error 500 do
